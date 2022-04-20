9a62a1d5e5500b6da4dc30d6f6b57840ccf5711a
Yefta <johanesyefta012@gmail.com>
2022-04-20 07:39:27 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { DonationComponent } from './donation/donation.component';
+import { HomeComponent } from './home/home.component';
+import { MovementComponent } from './movement/movement.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'donation', component: DonationComponent},
+  {path: 'movement', component: MovementComponent},
+  {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DonationComponent } from './donation/donation.component';
+import { MovementComponent } from './movement/movement.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DonationComponent,
+    MovementComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
